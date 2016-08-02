175dcbc417c8a4ed258045d06fe730a0588cd607
Jonathan Lam <jlam55555@gmail.com>
2016-08-02 16:35:59 -0400

    Fixed problem with multiple assignment with Ruby

--- src/scrambler.rb
+++ src/scrambler.rb
@@ -1,6 +1,7 @@
 turnTypes = [ "U", "R", "L", "D", "B", "F" ]
 revTypes = [ " ", "2", "'" ]
-prng, moves, numMoves, types, type = Random.new, [], 25, 0, turnTypes[prng.rand(6)]
+prng = Random.new
+moves, numMoves, types, type = [], 25, 0, turnTypes[prng.rand(6)]
 puts
 while types < numMoves do
   if types == 0 or moves[types-1] != (type = turnTypes[prng.rand(6)]) then
